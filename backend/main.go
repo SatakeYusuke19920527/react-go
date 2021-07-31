aba218a2b840b1aadf9eb253e377b3bdd8650b15
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-07-31 17:30:20 +0900

    commit

--- backend/main.go
+++ backend/main.go
@@ -10,7 +10,14 @@ func main() {
 	engine := gin.Default()
 	engine.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
+			"message":  "hello world",
+			"message1": "hello world",
+			"message2": "hello world",
+			"message3": "hello world",
+			"message4": "hello world",
+			"message5": "hello world",
+			"message6": "hello world",
+			"message7": "hello world",
 		})
 	})
 	engine.Run(":3001")
