3bd5e934176a77d7ff8590a0795d7d023df5979f
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-08-03 13:38:57 +0900

    commit

--- backend/main.go
+++ backend/main.go
@@ -1,32 +1,46 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-func reciever(name string, c chan int) {
-	for {
-		i, ok := <-c
-		if !ok {
-			break
-		}
-		fmt.Println(name, i)
-	}
-	fmt.Println(name + "END")
+type User struct {
+	Name string
+	Age  int
+}
+
+func UpdateUser(user User) {
+	user.Name = "update"
+	user.Age = 30
+}
+
+func UpdateUser2(user *User) {
+	user.Name = "update"
+	user.Age = 30
 }
 
 func main() {
-	ch1 := make(chan int, 2)
-	go reciever("1,goroutin", ch1)
-	go reciever("2,goroutin", ch1)
-	go reciever("3,goroutin", ch1)
+	var user1 User
+	fmt.Println(user1)
+	user1.Name = "user1"
+	user1.Age = 29
+	fmt.Println(user1)
 
-	i := 0
-	for i < 100 {
-		ch1 <- i
-		i++
-	}
-	close(ch1)
-	time.Sleep(300 * time.Millisecond)
+	user2 := User{"user2", 29}
+	fmt.Println(user2)
+
+	user3 := User{"user3", 29}
+	fmt.Println(user3)
+
+	user4 := User{"user4", 40}
+	fmt.Println(user4)
+
+	user5 := new(User)
+	fmt.Println(user5)
+	user6 := &User{}
+	fmt.Println(user6)
+
+	UpdateUser(user3)
+	UpdateUser2(&user4)
+
+	fmt.Println(user3)
+	fmt.Println(user4)
 }
